7073b312caea4d20842848bd074b7f63b2ca9b0f
86gumball <lucasruben2000@gmail.com>
2020-07-27 11:57:38 +0000

    Setup routes correctly and add view for clearance

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -2,5 +2,5 @@ Clearance.configure do |config|
   config.mailer_sender = "reply@example.com"
   config.rotate_csrf_on_sign_in = true
   config.allow_sign_up = true
-  config.routes = true
+  config.routes = false
 end
