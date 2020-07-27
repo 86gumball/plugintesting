4ab91eb6d8b1fd408c622037700fd5cf61f2a83d
86gumball <lucasruben2000@gmail.com>
2020-07-27 16:39:31 +0000

    Configure e-mail sender from "Forget Password"

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,6 +1,6 @@
 Clearance.configure do |config|
-  config.mailer_sender = "reply@example.com"
+  config.mailer_sender = "rubenrailsmailsender@gmail.com"
   config.rotate_csrf_on_sign_in = true
-  config.allow_sign_up = true
+  config.allow_sign_up = false
   config.routes = false
 end
