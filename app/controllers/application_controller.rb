bcd29b1bb7c515704f2dd934ef6c3834da01548b
86gumball <lucasruben2000@gmail.com>
2020-07-28 11:07:09 +0000

    Configure Post destroy permissions with pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
   include Clearance::Controller
+  include Pundit
   before_action :require_login
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+  def user_not_authorized
+    flash[:warning] = "You are not authorized to perform this action"
+    redirect_to(request.referrer || root_path)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
 
   validates :username, presence: true, uniqueness: true
 
+  has_many :posts
+
   def username
     self.email.split('@').first
   end
